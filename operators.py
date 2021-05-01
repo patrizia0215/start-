2ce45883ec3d602e6e0340a43d1f7030cef3e8ed
patrizia0215 <patriziakayebungubung@gmail.com>
2021-05-01 23:30:32 +0800

    start

---
+++ operators.py
@@ -0,0 +1,17 @@
+a = 2
+b = 3
+
+print(a * b)
+
+print(a ** b)
+
+print(a // b)
+
+print(a / b)
+
+print(a - b)
+
+print(a + b)
+
+
+
---
+++ string_concatenation.py
@@ -0,0 +1,18 @@
+first_name = 'pat'
+middle_name = 'senerez'
+last_name = 'bungubung'
+
+#python convention of string concatenation
+
+print(f'{first_name} {middle_name} {last_name}')
+
+
+#basic string concatenation
+print(first_name + ' ' + middle_name + ' ' + last_name)
+
+#string 
+print("My name is: %s %s %s" % (first_name, middle_name, last_name))
+
+#format function 
+print("My name is: {} {} {}".format(first_name, middle_name, last_name))
+
